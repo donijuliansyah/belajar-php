a6158159568a2a2288cb46e80fb68eb13e44f11e
Doni Juliansyah <donijoelian9@gmail.com>
2022-10-24 11:13:47 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World <br>";
+
+$nama = "Doni";
+$umur = 21 ;
+
+echo "nama saya <Strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Della" ;
+$umurAdik = 17 ;
+
+
+$selisihUmur = $umur-$umurAdik ;
+echo "nama adik saya <strong> $namaAdik</strong>, adik saya berusia $umurAdik tahun.<br> Selisih umur saya dan adik saya $selisihUmur";
+
+
+
+?>
