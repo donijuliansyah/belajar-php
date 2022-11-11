365b3d327aad3f8e17d2fa1f51e370c2b791539f
Doni Juliansyah <donijoelian9@gmail.com>
2022-11-11 10:35:38 +0700

    add form handling

--- action.php
+++ action.php
@@ -2,19 +2,41 @@
 
 if(isset [$_GET['p']) {
 
-    if ($_GET['p'] == 'tampil;'){
+    if ($_GET['p'] == 'tampil;'){ //standar
         
-    }
-echo $_POST['nama'] ;
-echo "<br/>" ;
-echo $_POST['password'] ;
+        echo $_POST['nama'] ;
+        echo "<br/>" ;
+        echo $_POST['password'] ;
 
-} else if ($_GET['p'] == "pesan"){
-    echo "Halo Apakabar" $_POST ['nama'];  
-    echo "br/"  ;
-    echo "password anda adalah". $_POST['password'] ;
-} else if
-    echo "anda tidak boleh mengakses halaman ini"
-    
-}else if ($_GET['p'] == "gambar"){
+} else if ($_GET['p'] == "pesan"){// ada pesannya
+        echo "Halo Apakabar" $_POST ['nama'];  
+        echo "br/"  ;
+        echo "password anda adalah". $_POST['password'] ;
+
+} else if ($_GET['p'] == "aman"){// tambahan 
+    if (!empty($_POST['nama'])) {
+        echo $_POST ['nama']
+    }else {
+        echo "nama belum dimasukkan";
+    }
+
+
+echo "<br/>";
+
+if (!empty( $_POST['password'])){
+    echo $_POST ['password'];
+} else {
+    echo "password masih kosong";
+}
+
+} else if ($_GET['p'] == "gambar") { // tampil gambar
+        $size = getimagesize($_FILES['berkas']['tmpname']); // mengambil size
+        $image = "data:".$size ['mime'].";base64,".
+                base64_encode(file_get_contents($_FILES['berkas']['tmp_name'])); // menampilkan gambar
+        echo "<image src='".$image."' width='720'>;
+    }
+
+} else {
+    echo "anda tidak boleh mengakses halaman ini";
+}
 ?>
